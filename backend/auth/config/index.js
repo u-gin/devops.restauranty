3df6dec916374b5076ca907afb2799b696c337ed
Eugene Asiedu <uginasiedu079@gmail.com>
2025-12-04 05:24:34 +0100

    file

--- backend/auth/config/index.js
+++ backend/auth/config/index.js
@@ -20,7 +20,7 @@ module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
-
+  
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
